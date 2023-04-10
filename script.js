d81b146edbf9a64ed6a303662704390afbaa4d0d
klphumisithikul <klphumisithikul@gmail.com>
2023-04-10 10:27:12 -0400

    add toggle seat

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat: note(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = movieSelect.value;
+const ticketPrice = +movieSelect.value;
 
-console.log(ticketPrice);
+container.addEventListener('click', e => {
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+    }
+})
