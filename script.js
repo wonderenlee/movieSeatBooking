2be59b03af103434da811f59f84a0b3afd6a3329
klphumisithikul <klphumisithikul@gmail.com>
2023-04-10 10:12:56 -0400

    add js const

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const seat = document.querySelectorAll('.row .seat: note(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = movieSelect.value;
+
+console.log(ticketPrice);
