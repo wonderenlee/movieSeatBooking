c608fe31bea594f4e8afa203e9e9ee7b665d0cee
klphumisithikul <klphumisithikul@gmail.com>
2023-04-11 09:20:04 -0400

    added eventlistener for tickets

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('. row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
 
 container.addEventListener('click', e=> {
     if (
