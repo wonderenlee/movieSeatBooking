926b291ecd3bb2cd8ba08b600e4103abf4f079a1
wonderenlee <wonderenlee@gmail.com>
2023-05-01 00:35:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,23 @@ function updateSelectedCount() {
 
 // Get data from local storage and populate UI
 function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
     
+    console.log(selectedSeats);
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 // Movie select event
 movieSelect.addEventListener('change', e => {
@@ -39,11 +55,17 @@ movieSelect.addEventListener('change', e => {
     updateSelectedCount();
 });
 
+// Seat click event
 container.addEventListener('click', e => {
     if (
         e.target.classList.contains('seat') &&
         !e.target.classList.contains('occupied')
     ) { 
-        e.target.classList.toggle('selected'); 
+        e.target.classList.toggle('selected');
+        
+        updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
